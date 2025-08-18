f1d1fea379c35bb169f23795600f925cf3af71ab
tarungarg18 <163766141+tarungarg18@users.noreply.github.com>
2025-08-18 21:45:41 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.REACT_APP_API_URL || "http://localhost:5000 || https://chat-app-9itg.onrender.com";
+export const host = process.env.REACT_APP_API_URL || "http://localhost:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 // OTP routes removed
